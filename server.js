7b06217bc842779d6f00ab44e30e83e3b9c0321e
Guilherme <pelegrinnibr@gmail.com>
2026-04-15 19:35:40 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(express.json());
+
+app.post('/criar-pagamento', async (req, res) => {
+  const dados = req.body;
+
+  const response = await fetch('https://api.petta.me/payments', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer SUA_API_KEY',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      amount: dados.valor,
+      customer: {
+        name: dados.nome,
+        document: dados.cpf
+      }
+    })
+  });
+
+  const result = await response.json();
+
+  res.json(result);
+});
+
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log('Rodando na porta 3000');
+});
